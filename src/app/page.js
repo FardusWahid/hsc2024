0ec79133b6903df03ad18493cd942e9372f11991
Fardus wahid <farduswahid87@gmail.com>
2024-05-19 22:06:27 +0600

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -16,9 +16,9 @@ const page = async (prop) => {
     <div>
 <section className='grid items-center m-auto justify-center w-3/4 mt-1 '>
       
-      <h1 className='text-center font-bold text-lg'>Hi there {session?.user?.name} </h1>
-      <p className='text-lg text-center my-2  font-mono font-extrabold tracking-tight'>Your account status: <span className='font-bold font-mono tracking-widest text-red-800 bg-white px-2 rounded-sm py-[2px] shadow'>{session?.user?.role}</span><br/></p>
-      {session?.user?.role == "unpaid"? <p className='text-center font-sans font-extrabold'>Pay <Link className='text-white bg-blue-950 tracking-widest font-extrabold px-2 py-[2px] rounded-md shadow-inner shadow-blue-500' href="https://www.facebook.com/free.courses.vercel/">here</Link> to continue</p>: <p className='font-mono text-center card p-2 tracking-tighter font-extrabold '>Thank you for trusting our service. Please Share to get Upto 100% Cashback <Link className='bg-purple-950 text-white px-2 py-[2px] rounded-md shadow-inner shadow-yellow-600 active:tracking-normal' href="">Explain How</Link> </p>}
+      <h1 className='text-center font-sans font-bold text-lg'>HI there <span className='font-sans font-extrabold tracking-tight'>{session?.user?.name}</span> </h1>
+      <p className='text-lg text-center my-2 font-sans font-extrabold tracking-tighter'>your account status: <span className='font-extrabold font-sans uppercase tracking-widest  px-2 rounded-sm py-[2px] shadow'>{session?.user?.role}</span><br/></p>
+      {session?.user?.role == "unpaid"? <p className='text-center font-sans font-extrabold'>Pay <Link className='text-white bg-blue-950 tracking-widest font-extrabold px-2 py-[2px] rounded-md shadow-inner shadow-blue-500' href="https://www.facebook.com/free.courses.vercel/">here</Link> to continue</p>: <p className='font-sans text-center p-2 tracking-tighter font-extrabold '>Get 50taka Cashback for every successful referral <Link className='bg-zinc-200 text-purple-950 px-1 py-[2px] rounded-sm shadow-inner font-sans shadow-gray-400 active:tracking-normal' href="https://www.facebook.com/free.courses.vercel/">Explain</Link> </p>}
       </section>
 
 
