044e2e4d44c32dfcb6aff08fd2ac7a5fb090fef5
Fardus wahid <farduswahid87@gmail.com>
2024-06-28 04:53:04 +0600

    newuser

--- src/app/component/post.jsx
+++ src/app/component/post.jsx
@@ -1,22 +1,23 @@
 import Image from "next/image"
+import Link from "next/link"
 
 function Post(props) {
   return (
     <div><br />
         
         <div 
-  className={`ring-1 ring-black rounded-lg w-11/12 md:w-[370px] lg:w-[450px] m-auto px-2 py-4 ${props.color}`}>
+  className={`ring-1 ring-black rounded-lg w-11/12 z-0 md:w-[370px] lg:w-[450px] m-auto px-2 py-4 ${props.color}`}>
 
     <div className="flex gap-1 mt-2">    
         <Image className="bg-white ring-1 ring-red-600 p-[5px] rounded-full" src="/aple.png" width={45} height={40}/>
         <section>
-        <p className="text-lg font-serif font-extrabold text-purple-950 tracking-tighter italic">@Apple ghost</p>
-        <p className="text-[11px] text-end font-mono font-extrabold italic tracking-widest">{props.date}</p>
+        <Link href='https://www.facebook.com/apple.courses.netlify.app.3.0/' className="text-lg font-sans font-extrabold tracking-wide italic">@Apple ghost</Link>
+        <p className="text-[11px] text-end font-sans font-bold italic tracking-widest">{props.date}</p>
         </section> 
 
     </div>
-    <h1 className="mt-5 font-sans text-lg italic text-start tracking-wide font-bold">{props.line}</h1>
-    <a href={props.road} className="text-sm font-sans font-extrabold underline text-blue-900">{props.name}</a>
+    <h1 className="mt-5 mb-1 font-sans text-lg  text-start tracking-normal font-bold">{props.line}</h1>
+    <a href={props.road} className="text-sm font-sans underline text-blue-800 hover:bg-lime-400 px-2 py-1 hover:no-underline rounded-sm hover:text-black bg-transparent backdrop-blur-xl bg-opacity-100 hover:shadow-md hover:shadow-gray-700 transition-all ease-in-out duration-150 font-extrabold">{props.name}</a>
 <img src={props.link} loading="lazy"  className="rounded-md mt-2 "/>
 </div>
 
--- src/app/updates/page.jsx
+++ src/app/updates/page.jsx
@@ -4,37 +4,42 @@ import Post from '../component/post'
 function page(props) {
   return (
     <div>
-<h1 className='text-center font-serif italic font-bold text-2xl '>Latest updates</h1>
+<h1 className='text-center font-sans font-extrabold tracking-wide text-2xl '>Latest updates</h1>
 
-<div className='flex flex-wrap justify-between  gap-5 md:mx-5 lg:mx-8 lg:justify-between'>
+<div className='flex flex-wrap justify-between gap-5 md:mx-5 lg:mx-8 lg:justify-between'>
+
+
+<Post
+date="28 june 2024"
+road="https://www.facebook.com/share/PotRZm2sKSjALm6P/"
+name="GROUP LINK"
+line="তোমাদের minor কোনো request such as, any E-Books, Digital products or any webapp feature etcetera প্রয়োজন হলে এই Facebook Group এ post করতে পারো"
+color='bg-[#ccff00] text-purple-950'
+/>
+
+
 <Post
 date="26 june 2024"
 road="https://www.facebook.com/groups/1451996508764221/permalink/1494593874504484/"
 name="POST LINK"
-line=" তোমাদের জন্য এটি Completely FREE! Apple Courses private group এর এই post এ Comment করো"
-link="https://asgcompressednote.com/assets/images/aa.jpg"
-color='bg-'
+line="ASG Compressed notes versison 2.0 তোমাদের জন্য Completely FREE! Apple Courses private group এর এই post এ Comment করো"
+color='bg-pink-300 text-lime-950'
 />
 
 <Post
 date="21 june 2024"
 road="https://www.facebook.com/apple.courses.netlify.app.3.0/"
 name="Facebook page"
-line="Follow Our New"
+line="Follow our new Facebook page"
 link="https://applecourses.netlify.app/_next/image?url=%2Fbetterfb.webp&w=1920&q=75"
-color='bg-amber-200'
+color='bg-cyan-200'
 />
 
-<Post
-date="19 june 2024"
-line="Facebook service is Unavailable for next 3 days. Message here for any potential question. https://t.me/applecourses"
-color='bg-pink-400'
-/>
 
 <Post
 date="12 june 2024"
 line="OneShot course এর ২টা missing ক্লাস Lecture Sheet এর সাথে দেয়া আছে "
-color='bg-sky-200'
+color='bg-sky-300'
 />
 
 
