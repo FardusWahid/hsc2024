f20759a9c63ce4f389b570e6fe04eae33aebaae2
Fardus wahid <farduswahid87@gmail.com>
2024-07-15 21:37:04 +0600

    fuckthituser

--- src/app/api/auth/[...nextauth]/option.js
+++ src/app/api/auth/[...nextauth]/option.js
@@ -12,7 +12,7 @@ export const option = {
 
             let userRole = "unpaid";
             const proUsers = ["farduswahid87@gmail.com","akashjr190@gmail.com","tanjirislamshishir@gmail.com", "ms1897473@gmail.com", "shahi76569@gmail.com","oodhora32@gmail.com","nahid01845654611@gmail.com","mostafizurvc2580@gmail.com","moinul12266@gmail.com","thorfinxx99@gmail.com","nomantui4321@gmail.com","ghoshavijit6749@gmail.com","muhammadtufazzal0@gmail.com"];
-            const preEmails = ["mdaraf1232@gmail.com","farduswahid31@gmail.com","hozukisuigestu6@gmail.com","ssindidahmednabil@gmail.com","jawataafnan734@gmail.com","hscstudy0@gamil.com"];
+            const preEmails = ["mdaraf1232@gmail.com","farduswahid31@gmail.com","hozukisuigestu6@gmail.com","ssindidahmednabil@gmail.com","jawataafnan734@gmail.com","hscstudy47@gamil.com"];
 
 
             if(profile?.email && preEmails.includes(profile.email)){
