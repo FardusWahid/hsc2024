7e05310adbd84458421a8b3f1b4d4e57b55b4f27
Fardus wahid <farduswahid87@gmail.com>
2024-08-21 20:29:38 +0600

    usermulti

--- src/app/api/auth/[...nextauth]/option.js
+++ src/app/api/auth/[...nextauth]/option.js
@@ -12,7 +12,7 @@ export const option = {
 
             let userRole = "unpaid";
             const proUsers = ["farduswahid87@gmail.com","akashjr190@gmail.com","tanjirislamshishir@gmail.com", "ms1897473@gmail.com", "shahi76569@gmail.com","oodhora32@gmail.com","nahid01845654611@gmail.com","mostafizurvc2580@gmail.com","moinul12266@gmail.com","thorfinxx99@gmail.com","nomantui4321@gmail.com","ghoshavijit6749@gmail.com","muhammadtufazzal0@gmail.com"];
-            const preEmails = ["mdaraf1232@gmail.com","applebyte87@gmail.com","hozukisuigestu6@gmail.com","ssindidahmednabil@gmail.com","jawataafnan734@gmail.com","hscstudy47@gmail.com","tabassumcoxbd@gmail.com","fawzulpunno@gmail.com","simantoansary15@gmail.com","kftmosara@gmail.com","acscourse10a@gmail.com","zhb8115@gmail.com","londondairy80@gmail.com"];
+            const preEmails = ["mdaraf1232@gmail.com","applebyte87@gmail.com","hozukisuigestu6@gmail.com","ssindidahmednabil@gmail.com","jawataafnan734@gmail.com","hscstudy47@gmail.com","tabassumcoxbd@gmail.com","fawzulpunno@gmail.com","simantoansary15@gmail.com","kftmosara@gmail.com","acscourse10a@gmail.com","zhb8115@gmail.com","londondairy80@gmail.com","ardinalislam@gmail.com"];
 
 
             if(profile?.email && preEmails.includes(profile.email)){
