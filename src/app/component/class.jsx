a954432d94c42ae3e631208ae54f0cb94c5d90bd
FardusWahid <158212453+FardusWahid@users.noreply.github.com>
2024-10-02 02:44:59 -0700

    Update class.jsx

--- src/app/component/class.jsx
+++ src/app/component/class.jsx
@@ -1,23 +1,21 @@
 'use client'
 import React, { useEffect, useState } from 'react';
 import ReactPlayer from 'react-player';
-import { useMediaQuery } from 'react-responsive';
+import LoadingScreen from './loading';
 
 const API_KEY = 'AIzaSyBRxPt8NOlqBUKrNvUtHeuCjs9Z3MWUzrs';
-const videosPerPage = 9; 
-function Neo(props) {
+const videosPerPage = 12; 
+
+function Neo({ playid, chapter }) {
   const [videos, setVideos] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
-  const isLargeScreen = useMediaQuery({ query: '(min-width: 768px)' });
-  const maxLength =100;
-
 
   useEffect(() => {
     const fetchPlaylist = async () => {
       try {
-        const response = await fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${props.playid}&key=${API_KEY}&maxResults=150`);
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playid}&key=${API_KEY}&maxResults=150`);
         const data = await response.json();
 
         if (!response.ok) {
@@ -36,49 +34,53 @@ function Neo(props) {
   }, []);
 
   const paginate = pageNumber => setCurrentPage(pageNumber);
-
   const indexOfLastVideo = currentPage * videosPerPage;
   const indexOfFirstVideo = indexOfLastVideo - videosPerPage;
   const currentVideos = videos.slice(indexOfFirstVideo, indexOfLastVideo);
+
   return (
     <div className='select-none'>
-
-      <h1 className='text-center font-extrabold mt-2 md:mt-0 text-2xl'>{props.chapter}</h1><br />
-
+      <h1 className='text-center font-sans tracking-tight text-cyan-200 font-extrabold mt-4 md:mt-[-5px] text-2xl'>{chapter}</h1>
+      <br />
       {loading && <p className=" flex justify-center items-center font-extrabold tracking-tight text-lg text-white">Loading...</p>}
       {error && <p>Error: {error}</p>}
       {!loading && !error && (
-        <div className="w-full flex flex-wrap justify-center 2xl:w-[1490px] 2xl:m-auto 2xl:justify-start gap-5">
+        <div className="flex flex-wrap w-full justify-center gap-3">
           {currentVideos.map(video => (
 
-  <div key={video.id}>
-  <p className='m-auto text-center text-[13px] mb-1 tracking-tighter font-bold font-sans w-[344px] lg:w-[477px] truncate whitespace-nowrap overflow-hidden active:whitespace-normal active:overflow-visible active:text-clip'>{video.snippet.title}</p>
-
-
-
-<div className="bg-gray-950 lg:w-[480px]">
-              <section className='w-full flex justify-center'>
+            <div key={video.id} className="w-[99%] md:w-[95%] lg:w-[32.55%]">
+              <p className="text-center text-cyan-200 font-sans italic text-sm md:text-[15px] font-extrabold bg-gray-950 truncate overflow-hidden mb-1 py-1 whitespace-nowrap active:whitespace-normal active:overflow-visible active:text-clip">
+                {video.snippet.title}
+              </p>
+              <div className="relative pb-[56.25%]">
                 <ReactPlayer
                   url={`https://www.youtube.com/watch?v=${video.snippet.resourceId.videoId}`}
-                  controls={true}
-                  width={isLargeScreen ? 475 : 344}
-                  height={isLargeScreen ? 290 : 220}
+                  controls
+                  width="100%"
+                  height="100%"
                   light={`https://img.youtube.com/vi/${video.snippet.resourceId.videoId}/hqdefault.jpg`}
-                 playing={true}
-                 
-className="ring-[0.6px] ring-gray-800"
-                
-                />
+                  playing
+                  className="absolute border-2 border-gray-900 rounded-md"
 
-              </section></div>
+                />
+              </div>
             </div>
           ))}
         </div>
       )}
-      {/* Pagination */}
+      
+      
       <div className="flex justify-center m-8 gap-2">
         {[...Array(Math.ceil(videos.length / videosPerPage))].map((_, index) => (
-          <button key={index} onClick={() => paginate(index + 1)} className={`mx-1 italic px-3 py-1 rounded-md ${currentPage === index + 1 ? 'bg-white ring-1 ring-white text-black font-extrabold' : 'bg-black ring-white ring-1 text-white  font-extrabold'}`}>
+          <button
+            key={index}
+            onClick={() => paginate(index + 1)}
+            className={`mx-1 italic px-[11px] py-1 font-sans font-extrabold rounded-[5px]  ${
+              currentPage === index + 1
+                ? 'bg-cyan-100 text-black'
+                : 'bg-gray-900 text-white shadow-[0px_0px_8px] hover:shadow-none shadow-cyan-300'
+            }`}
+          >
             {index + 1}
           </button>
         ))}
